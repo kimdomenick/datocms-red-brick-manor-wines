701622c737df797c1077b50eaa1bfd967f6b78d7
kimdomenick <kdomenick@thinkoomph.com>
2022-10-04 12:37:43 -0400

    Test

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,15 +5,6 @@ export default function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4">
-            WHO
-          </h3>
-          <p>
-            Built by{" "}
-            <a href="https://www.fijiggle.com" target="_blank">
-              Fijiggle.com
-            </a>
-          </p>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4">
               WHY
